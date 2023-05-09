38464890ddb911549d9f65f2f380a75a78893f51
GitHub Action <action@github.com>
2023-05-09 10:31:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/innocentuslime/jbuxintern/services/MyProjectService.kt
+++ src/main/kotlin/com/github/innocentuslime/jbuxintern/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.innocentuslime.jbuxintern.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.innocentuslime.jbuxintern.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
