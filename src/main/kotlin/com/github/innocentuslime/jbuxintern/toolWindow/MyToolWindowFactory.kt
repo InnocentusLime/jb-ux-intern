d2409972f58b14a89d92f9f228416c3aa70f9a76
InnocentusLime <InnocentusLime.help@gmail.com>
2023-05-12 18:15:21 +0300

    Now counting for all class files

--- src/main/kotlin/com/github/innocentuslime/jbuxintern/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/innocentuslime/jbuxintern/toolWindow/MyToolWindowFactory.kt
@@ -1,16 +1,17 @@
 package com.github.innocentuslime.jbuxintern.toolWindow
 
 import com.github.innocentuslime.jbuxintern.MyBundle
+import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.diagnostic.thisLogger
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.psi.JavaRecursiveElementVisitor
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiManager
+import com.intellij.psi.search.FileTypeIndex
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
@@ -35,17 +36,28 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     class MyToolWindow(toolWindow: ToolWindow) {
 
-        private val editorManager = FileEditorManager.getInstance(toolWindow.project)
-        private val docManager = FileDocumentManager.getInstance()
         private val psiManager = PsiManager.getInstance(toolWindow.project)
+        private val projectScope = GlobalSearchScope.projectScope(toolWindow.project)
 
-        private fun getPsiFile(): PsiFile? {
-            val currentDoc = editorManager.selectedTextEditor?.document
-            val currentFile = if (currentDoc != null) docManager.getFile(currentDoc) else null
+        private fun countClasses(cl: PsiClass): Int {
+            var counter = 0
+            cl.accept(object : JavaRecursiveElementVisitor() {
+                override fun visitClass(aClass: PsiClass?) {
+                    counter += 1
+                    if (aClass == null) {
+                        return
+                    }
 
-            currentFile?.let { return psiManager.findFile(it) }
+                    aClass.allInnerClasses.forEach { x -> x.accept(this) }
+                    aClass.allMethods.forEach { x -> x.accept(this) }
+                }
+            })
 
-            return null
+            return counter
+        }
+
+        private fun classesInFile(cls: Array<PsiClass>): Int {
+            return cls.sumOf { x -> countClasses(x) }
         }
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
@@ -54,19 +66,16 @@ class MyToolWindowFactory : ToolWindowFactory {
             add(label)
             add(JButton(MyBundle.message("countClasses")).apply {
                 addActionListener {
-                    var counter = 0
-                    getPsiFile()?.accept(object : JavaRecursiveElementVisitor() {
-                        override fun visitClass(aClass: PsiClass?) {
-                            counter += 1
-                            if (aClass == null) {
-                                return
-                            }
+                    FileTypeIndex.getFiles(
+                        JavaFileType.INSTANCE,
+                        projectScope,
+                    ).stream()
+                        .map(psiManager::findFile)
+                        .filter { it is PsiJavaFile }
+                        .map { it as PsiJavaFile }
+                        .forEach { file -> thisLogger().warn("file ${file.name} has ${classesInFile(file.classes)} classes") }
 
-                            aClass.allInnerClasses.forEach { x -> x.accept(this) }
-                            aClass.allMethods.forEach { x -> x.accept(this) }
-                        }
-                    })
-                    label.text = MyBundle.message("classCount", counter)
+                    //label.text = MyBundle.message("classCount", counter)
                 }
             })
         }
