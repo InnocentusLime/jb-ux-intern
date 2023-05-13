d0088ddaa384efd1831b6fd626fb301225013e84
InnocentusLime <InnocentusLime.help@gmail.com>
2023-05-13 09:44:02 +0300

    Added display via tree

--- src/main/kotlin/com/github/innocentuslime/jbuxintern/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/innocentuslime/jbuxintern/toolWindow/MyToolWindowFactory.kt
@@ -1,6 +1,5 @@
 package com.github.innocentuslime.jbuxintern.toolWindow
 
-import com.github.innocentuslime.jbuxintern.MyBundle
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -9,11 +8,11 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.psi.*
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.treeStructure.Tree
 import com.intellij.util.containers.stream
-import javax.swing.JButton
+import javax.swing.tree.DefaultMutableTreeNode
 
 
 class MyToolWindowFactory : ToolWindowFactory {
@@ -64,31 +63,36 @@ class MyToolWindowFactory : ToolWindowFactory {
             return Pair(classCounter, funcCounter)
         }
 
-        private fun classesInFile(cls: Array<PsiClass>): Pair<Int, Int> {
-            return cls.stream().map { x -> countClassesAndFunctions(x) }
+        private fun classesAndMethodsInFile(file: PsiJavaFile): Pair<Int, Int> {
+            return file.classes.stream().map { x -> countClassesAndFunctions(x) }
                 .reduce(Pair(0, 0)) {
                     (x1, y1), (x2, y2) -> Pair(x1 + x2, y1 + y2)
                 }
         }
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("classCount", "?"))
+            val root = DefaultMutableTreeNode("Class and method count")
+            val tree = Tree(root)
 
-            add(label)
-            add(JButton(MyBundle.message("countClasses")).apply {
-                addActionListener {
-                    FileTypeIndex.getFiles(
-                        JavaFileType.INSTANCE,
-                        projectScope,
-                    ).stream()
-                        .map(psiManager::findFile)
-                        .filter { it is PsiJavaFile }
-                        .map { it as PsiJavaFile }
-                        .forEach { file -> thisLogger().warn("file ${file.name} info: ${classesInFile(file.classes)}") }
+            FileTypeIndex.getFiles(
+                JavaFileType.INSTANCE,
+                projectScope,
+            ).stream()
+                .map(psiManager::findFile)
+                .filter { it is PsiJavaFile }
+                .map { it as PsiJavaFile }
+                .forEach { file ->
+                    val fileNode = DefaultMutableTreeNode(file.name)
+                    val (classCount, methodCount) = classesAndMethodsInFile(file)
+                    val classNode = DefaultMutableTreeNode("Class count: $classCount")
+                    val methodNode = DefaultMutableTreeNode("Method count: $methodCount")
 
-                    //label.text = MyBundle.message("classCount", counter)
+                    fileNode.add(classNode)
+                    fileNode.add(methodNode)
+                    root.add(fileNode)
                 }
-            })
+
+            add(tree)
         }
     }
 }
