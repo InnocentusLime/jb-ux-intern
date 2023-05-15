d1ddb08d198e4dd9e8a0e48bd35f60e2020d8e2b
InnocentusLime <InnocentusLime.help@gmail.com>
2023-05-15 10:45:22 +0300

    Improvements to the API

--- src/main/kotlin/com/github/innocentuslime/jbuxintern/toolWindow/ClassCountWindowFactory.kt
+++ src/main/kotlin/com/github/innocentuslime/jbuxintern/toolWindow/ClassCountWindowFactory.kt
@@ -12,6 +12,7 @@ import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.treeStructure.Tree
 import com.intellij.util.containers.stream
+import java.util.stream.Collectors
 import javax.swing.tree.DefaultMutableTreeNode
 
 
@@ -19,73 +20,81 @@ class ClassCountWindowFactory : ToolWindowFactory {
     private val contentFactory = ContentFactory.SERVICE.getInstance()
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
+        val myToolWindow = MyToolWindow(classesAndMethodsInProject(project))
         val content = contentFactory.createContent(myToolWindow.getContent(), null, false)
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project) = true
 
-    class MyToolWindow(toolWindow: ToolWindow) {
-
-        private val psiManager = PsiManager.getInstance(toolWindow.project)
-        private val projectScope = GlobalSearchScope.projectScope(toolWindow.project)
-
-        private fun countClassesAndFunctions(cl: PsiClass): Pair<Int, Int> {
-            var classCounter = 0
-            var funcCounter = 0
-            cl.accept(object : JavaRecursiveElementVisitor() {
-                override fun visitClass(aClass: PsiClass?) {
-                    classCounter += 1
-                    if (aClass == null) {
-                        return
-                    }
-
-                    aClass.innerClasses.forEach { x -> x.accept(this) }
-                    aClass.methods.forEach { x -> x.accept(this) }
-                }
-
-                override fun visitMethod(method: PsiMethod?) {
-                    funcCounter += 1
-                    if (method == null) {
-                        return
-                    }
-
-                    method.body?.accept(this)
-                }
-            })
-
-            return Pair(classCounter, funcCounter)
-        }
-
-        private fun classesAndMethodsInFile(file: PsiJavaFile): Pair<Int, Int> {
-            return file.classes.stream().map { x -> countClassesAndFunctions(x) }
-                .reduce(Pair(0, 0)) {
-                    (x1, y1), (x2, y2) -> Pair(x1 + x2, y1 + y2)
-                }
-        }
+    private class MyToolWindow(private val view: Map<String, Pair<Int, Int>>) {
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
             val root = DefaultMutableTreeNode(MyBundle.message("classAndMethodCountHeading"))
             val tree = Tree(root)
 
-            FileTypeIndex.getFiles(
-                JavaFileType.INSTANCE,
-                projectScope,
-            ).stream()
-                .map(psiManager::findFile)
-                .filter { it is PsiJavaFile }
-                .map { it as PsiJavaFile }
-                .forEach { file ->
-                    val fileNode = DefaultMutableTreeNode(file.name)
-                    val (classCount, methodCount) = classesAndMethodsInFile(file)
+            view.forEach { (name, counts) ->
+                val (classCount, methodCount) = counts
+                val fileNode = DefaultMutableTreeNode(name)
 
-                    fileNode.add(DefaultMutableTreeNode(MyBundle.message("classCount", classCount)))
-                    fileNode.add(DefaultMutableTreeNode(MyBundle.message("methodCount", methodCount)))
-                    root.add(fileNode)
-                }
+                fileNode.add(DefaultMutableTreeNode(MyBundle.message("classCount", classCount)))
+                fileNode.add(DefaultMutableTreeNode(MyBundle.message("methodCount", methodCount)))
+                root.add(fileNode)
+            }
 
             add(tree)
         }
     }
+}
+
+
+private fun countClassesAndFunctions(cl: PsiClass): Pair<Int, Int> {
+    var classCounter = 0
+    var funcCounter = 0
+    cl.accept(object : JavaRecursiveElementVisitor() {
+        override fun visitClass(aClass: PsiClass?) {
+            classCounter += 1
+            if (aClass == null) {
+                return
+            }
+
+            aClass.innerClasses.forEach { x -> x.accept(this) }
+            aClass.methods.forEach { x -> x.accept(this) }
+        }
+
+        override fun visitMethod(method: PsiMethod?) {
+            funcCounter += 1
+            if (method == null) {
+                return
+            }
+
+            method.body?.accept(this)
+        }
+    })
+
+    return Pair(classCounter, funcCounter)
+}
+
+private fun classesAndMethodsInFile(file: PsiJavaFile): Pair<Int, Int> {
+    return file.classes.stream().map { x -> countClassesAndFunctions(x) }
+        .reduce(Pair(0, 0)) {
+                (x1, y1), (x2, y2) -> Pair(x1 + x2, y1 + y2)
+        }
+}
+
+fun classesAndMethodsInProject(project: Project): Map<String, Pair<Int, Int>> {
+    val psiManager = PsiManager.getInstance(project)
+    val projectScope = GlobalSearchScope.projectScope(project)
+
+    return FileTypeIndex.getFiles(
+        JavaFileType.INSTANCE,
+        projectScope,
+    ).stream()
+        .map(psiManager::findFile)
+        .filter { it is PsiJavaFile }
+        .map { it as PsiJavaFile }
+        .collect(Collectors.toMap(
+            { it.name },
+            { classesAndMethodsInFile(it) },
+        ))
 }
---
+++ src/test/testData/counting/myClass.java
@@ -0,0 +1,33 @@
+public class myClass {
+    class myClass1 {
+    }
+
+    class myClass2 {
+    }
+
+    class myClass3 {
+        void mySecretMethod() {
+            class SuperDeepClass {
+
+            }
+        }
+    }
+
+    void method() {
+        class MethodClass {
+
+        }
+    }
+}
+
+class myClass1 {
+}
+
+class myClass2 {
+}
+
+class myClass3 {
+}
+
+class myClass4 {
+}
