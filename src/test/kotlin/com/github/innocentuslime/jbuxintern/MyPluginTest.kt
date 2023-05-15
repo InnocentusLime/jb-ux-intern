f269ee4ad99445931ee9909c8c75b5c3b973283c
InnocentusLime <InnocentusLime.help@gmail.com>
2023-05-15 10:45:33 +0300

    Added tesr

--- src/test/kotlin/com/github/innocentuslime/jbuxintern/MyPluginTest.kt
+++ src/test/kotlin/com/github/innocentuslime/jbuxintern/MyPluginTest.kt
@@ -1,39 +1,20 @@
 package com.github.innocentuslime.jbuxintern
 
-import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
-import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.util.PsiErrorElementUtil
-import com.github.innocentuslime.jbuxintern.services.MyProjectService
+import com.github.innocentuslime.jbuxintern.toolWindow.classesAndMethodsInProject
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
 
-    fun testXMLFile() {
-        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
+    fun testCounting() {
+        myFixture.copyFileToProject("myClass.java")
+        myFixture.copyFileToProject("myNewClass.java")
+        val map = classesAndMethodsInProject(project)
 
-        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
-
-        assertNotNull(xmlFile.rootTag)
-
-        xmlFile.rootTag?.let {
-            assertEquals("foo", it.name)
-            assertEquals("bar", it.value.text)
-        }
+        assert(map["myClass.java"]?.let { it.first == 10 && it.second == 2 }!!)
+        assert(map["myNewClass.java"]?.let { it.first == 1 && it.second == 0 }!!)
     }
 
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-
-    fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
-
-        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
+    override fun getTestDataPath() = "src/test/testData/counting"
 }
